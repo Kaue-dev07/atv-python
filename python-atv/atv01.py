62c8cb0f9c311c0ea9581b1e1cb0b1339ce8da31
Kauê Rosa <kaue_rosa150@estudante.sesisenai.org.br>
2025-07-23 23:55:37 -0300

    Update atv01.py

--- python-atv/atv01.py
+++ python-atv/atv01.py
@@ -1,4 +1,6 @@
 nome = input("Seu nome aqui: ")
 
-print(f"Bem vindo!!!, seu nome é {nome}!")
+idade = input("Sua idade aqui: ")
+
+print(f"Bem vindo!!!, seu nome é {nome}! e sua idade é {idade} anos!")
 
