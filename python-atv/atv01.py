816a8325845addfa177e38ae390ae6c8dbaff4e0
Kauê Rosa <kaue_rosa150@estudante.sesisenai.org.br>
2025-08-11 11:18:08 -0300

    Update atv01.py

--- python-atv/atv01.py
+++ python-atv/atv01.py
@@ -1,5 +1,26 @@
-nome = input("Seu nome aqui: ")
+#01 - Furto do banco
 
-idade = input("Sua idade aqui: ")
+import random
 
-print(f"Bem vindo!!!, seu nome é {nome}! e sua idade é {idade} anos!")
+senha = random.randint(0, 9)
+
+print("Você está roubando um banco e tem 3 chances para acertar a senha de 0 a 9.")
+
+while True:
+
+    numero = int(input("Tente um número: "))
+
+    tentativas = 0
+
+    tentativas += 1
+
+    if numero == senha:
+
+        print("Você é um baita de um cagão e acertou a senha")
+    else:
+
+        tentativas == 3
+
+        print("Você errou a senha e foi preso")
+
+        break
